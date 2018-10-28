a800b842253fc1e08d1b089651bd4ef21dbd2893
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-28 15:29:52 +0100

    Added global variables to script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // DOM variables
 let input = document.getElementById("input");
 let result = document.getElementById("result");
+// global variables
+let alphabet = ['abcdefghijklmnopqrstuvwxyz'];
+let userInput = [];
+let userTime;
+// variables to calculate how long it takes for user to type the whole alphabet
+let a, b, c, d;
